bb201587712d6a9b9ddd39358349ef7f2757ebae
Chris Boertien <chris.boertien@bz-labs.com>
2011-12-14 16:51:16 -0700

    connect ajax form loading and some cleanup
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

--- app/overrides/view_decorator.rb
+++ app/overrides/view_decorator.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "admin/shared/_product_sub_menu",
                      :name => "batch_products_admin_product_sub_tabs",
-                     :insert_bottom => "[data-hook='admin_product_sub_tabs'], #admin_product_sub_tabs[data-hook]",
+                     :insert_bottom => "[data-hook='admin_product_sub_tabs']",
                      :text => "<%= tab(:product_datasheets, :label => :batch_updates) %>")
 
